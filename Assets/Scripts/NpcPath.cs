505c38c2dfc30d00c56feb3b09ce5a5258007e9a
Chris Lennon <leapyearboy@gmail.com>
2016-04-16 22:04:30 +0200

    moves camera to focused animated zone

--- Assets/Scripts/NpcPath.cs
+++ Assets/Scripts/NpcPath.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 
 public class NpcPath : MonoBehaviour {
+    Rigidbody2D playerBody;
 
-	// Use this for initialization
-	void Start () {
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 1, "speed", 8, "easetype","linear"));
-	}
+    // Use this for initialization
+    void Start () {
+        // Add a 2D rigid body so we can detect collisions
+        playerBody = gameObject.AddComponent<Rigidbody2D>();
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath0"), "delay", 0, "speed", 4, "easetype","linear"));
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 0, "speed", 8, "easetype", "linear"));
+    }
 	
 	// Update is called once per frame
 	void Update () {
