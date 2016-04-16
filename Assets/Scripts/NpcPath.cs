d76b90fd61c6b3d22fdf0e571e13d81fc1a296c7
Chris Lennon <leapyearboy@gmail.com>
2016-04-16 22:55:59 +0200

    adds final path

--- Assets/Scripts/NpcPath.cs
+++ Assets/Scripts/NpcPath.cs
@@ -8,8 +8,10 @@ public class NpcPath : MonoBehaviour {
     void Start () {
         // Add a 2D rigid body so we can detect collisions
         playerBody = gameObject.AddComponent<Rigidbody2D>();
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath0"), "delay", 0, "speed", 4, "easetype","linear"));
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 0, "speed", 8, "easetype", "linear"));
+
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath0"), "delay", 0, "time", 4, "easetype", "linear"));
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 4, "time", 4, "easetype", "linear"));
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath2"), "delay", 8, "time", 4, "easetype", "linear"));
     }
 	
 	// Update is called once per frame
