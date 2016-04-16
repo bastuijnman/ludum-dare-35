64fba3b30aa39deff7efd9d22f4547fc5747330d
Chris Lennon <leapyearboy@gmail.com>
2016-04-16 20:13:51 +0200

    adds tweening, sample level0, sample path, box item for pickup and script for sprite flipping

---
+++ Assets/Scripts/FlipBlock.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipBlock : MonoBehaviour
+{
+    public Sprite[] blockColors; //Assign the length of the array in the inspector,
+                                 //and drag the sprites you want it to change to into each element.
+    public int currentSprite = -1; //The int that tells us which sprite in the array
+                                   //we will use.
+    public bool deleting; //Unity doesn't actually delete an object until the next
+                          //frame, so this will allow us to make sure the code doesn't break by having
+                          //currentSprite be longer than the array length.
+    private int arrayLength;
+
+    void Start()
+    {
+        arrayLength = blockColors.Length - 1;
+    }
+
+    void OnCollisionEnter2D()
+    {
+        currentSprite += 1; // adds 1 to the "currentSprite" integer.
+
+        if (currentSprite > arrayLength)
+        { //Deletes the object if currentSprite is greater than the
+          //length of the blockColors array
+            deleting = true;
+            GameObject.Destroy(gameObject);
+        }
+        if (!deleting)
+        {
+            GetComponent<SpriteRenderer>().sprite = blockColors[currentSprite]; //sets the SpriteRenderer's
+                                                                                //Sprite to be the desired sprite in the blockColors array.
+        }
+    }
+}
---
+++ Assets/Scripts/NpcPath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcPath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 1, "time", 15));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
