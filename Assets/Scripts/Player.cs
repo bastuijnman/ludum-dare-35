1639c2572bcd2f1217477bb7346139b8f2ab3e9e
BJJLangedijk <bjjlangedijk@gmail.com>
2016-04-16 20:43:11 +0200

    add leftShift for transforming shapes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,28 +6,38 @@ public class Player : MonoBehaviour {
 	// Movement speed, increase or decrease for faster movement
 	public float speed = 3.0f;
 
-	// Collision radius, change this property to play around with collision detection
-	public float collisionRadius = 0.5f;
-
 	// Force at which the player can jump
 	public float jumpForce = 3.0f;
 
 	bool playerIsFalling = false;
 	Vector3 movement;
-	CircleCollider2D playerCollider;
-	Rigidbody2D playerBody;
+	CircleCollider2D circleCollider;
+    BoxCollider2D boxCollider;
 
+    Transform transform;
+    Rigidbody2D playerBody;
+    SpriteImage spriteImageScript;
 
-	void Start () {
+    public Sprite block;
+    public Sprite circle;
+    public Sprite line;
 
-		// Add a 2D rigid body so we can detect collisions
-		playerBody = gameObject.AddComponent<Rigidbody2D> ();
+    public int spriteIndex = 0;
+    public int availableTransforms = 3;
 
-		// Setup the 2D circle collider to detect collisions against the TileMap
-		playerCollider = gameObject.AddComponent<CircleCollider2D> ();
-		playerCollider.offset = Vector3.zero;
-		playerCollider.radius = collisionRadius;
-		playerCollider.isTrigger = true;
+    void Start () {
+
+        transform = gameObject.GetComponent<Transform> ();
+
+        spriteImageScript = gameObject.AddComponent<SpriteImage> ();
+
+        spriteImageScript.spriteImage = block;
+
+        // Add a 2D rigid body so we can detect collisions
+        playerBody = gameObject.AddComponent<Rigidbody2D> ();
+
+        // Setup the 2D circle collider to detect collisions against the TileMap
+        boxCollider = gameObject.AddComponent<BoxCollider2D> ();
 
 	}
 
@@ -45,10 +55,53 @@ public class Player : MonoBehaviour {
 		if (Input.GetButtonDown ("Jump") && !playerIsFalling) {
 			playerBody.velocity = new Vector2 (0, jumpForce);
 			playerIsFalling = true;
-		}
+        }
 
-		// Add to the current object position by updating movement with speed and game speed
-		transform.position += movement * speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            spriteIndex++;
+
+            if (spriteIndex == availableTransforms)
+            {
+                spriteIndex = 0;
+            }
+
+            DestroyImmediate(gameObject.GetComponent<CircleCollider2D>());
+            DestroyImmediate(gameObject.GetComponent<BoxCollider2D>());
+
+            switch (spriteIndex)
+            {
+                case 0:
+                    spriteImageScript.spriteImage = block;
+
+                    boxCollider = gameObject.AddComponent<BoxCollider2D>();
+
+                    transform.localScale = new Vector3(2f, 2f, 1f);
+                    break;
+                case 1:
+                    spriteImageScript.spriteImage = circle;
+
+                    circleCollider = gameObject.AddComponent<CircleCollider2D>();
+                    circleCollider.offset = Vector3.zero;
+                    circleCollider.radius = 0.3f;
+
+                    transform.localScale = new Vector3(1f, 1f, 1f);
+                    break;
+                case 2:
+                    spriteImageScript.spriteImage = line;
+
+                    transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+
+                    transform.localScale = new Vector3(1f, 4f, 1f);
+
+                    boxCollider = gameObject.AddComponent<BoxCollider2D>();
+                    break;
+
+            }
+        }
+
+        // Add to the current object position by updating movement with speed and game speed
+        transform.position += movement * speed * Time.deltaTime;
 	
 	}
 
---
+++ Assets/Scripts/SpriteImage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteImage : MonoBehaviour
+{
+
+    public Sprite spriteImage;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Update()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // we are accessing the SpriteRenderer that is attached to the Gameobject
+        if (spriteRenderer.sprite != spriteImage) // if the sprite on spriteRenderer is null then
+            spriteRenderer.sprite = spriteImage; // set the sprite to sprite1
+    }
+
+}
