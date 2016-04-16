8855e403ac52ae9e19df01622246c69889f9a079
Chris Lennon <leapyearboy@gmail.com>
2016-04-16 20:52:50 +0200

    adds ... stuff

--- Assets/Scripts/NpcPath.cs
+++ Assets/Scripts/NpcPath.cs
@@ -5,7 +5,7 @@ public class NpcPath : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 1, "time", 15));
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("NpcPath1"), "delay", 1, "speed", 8, "easetype","linear"));
 	}
 	
 	// Update is called once per frame
