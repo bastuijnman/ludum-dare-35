ec43c575d4a378d4f66352011d5b15957e3ac975
Bas Tuijnman <bas.tuijnman@24i.com>
2016-04-17 21:35:26 +0200

    changes to gameover screen

--- Assets/Scripts/Screens/GameOverScreen.cs
+++ Assets/Scripts/Screens/GameOverScreen.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public class GameOverScreen : MonoBehaviour
 {
 
+	// Start our coroutine
 	void Start () {
 		StartCoroutine (WaitAndReload ());
 	}
 
+	// Reload the active level
 	public IEnumerator WaitAndReload () {
 		yield return new WaitForSeconds (5);
 		GameSceneManager.Instance.RestartScene ();
 	}
 
+	// Draw the Game Over screen
 	void OnGUI () {
 		GUI.DrawTexture (
 			new Rect (0, 0, Screen.width, Screen.height), 
