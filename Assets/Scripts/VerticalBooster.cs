c08bc81eec9836ab91d9fb3da0eb463398461649
Bas Tuijnman <bas.tuijnman@24i.com>
2016-04-18 09:34:39 +0200

    Fix level 10

--- Assets/Scripts/VerticalBooster.cs
+++ Assets/Scripts/VerticalBooster.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class VerticalBooster : MonoBehaviour {
 
+	public float playerVelocity;
+
 	BoxCollider2D endpointCollider;
 
 	// Initalises the LevelEndpoint
 	void Start () {
+		CreateCollider ();
+	}
+
+	void CreateCollider () {
 		endpointCollider = gameObject.AddComponent<BoxCollider2D> ();
 		endpointCollider.offset = Vector3.zero;
 	}
@@ -14,11 +20,13 @@ public class VerticalBooster : MonoBehaviour {
 	// When a player collides we need to load the next level
 	void OnCollisionEnter2D (Collision2D col) {
 		if (col.gameObject.tag == "Player") {
+			
 			Destroy (gameObject);
+
 			GameSceneManager manager = GameSceneManager.Instance;
 			Player p = col.gameObject.GetComponent<Player> ();
 
-			p.body.velocity = new Vector2(0, 60);
+			p.body.velocity = new Vector2(0, playerVelocity);
 
 		}
 	}
